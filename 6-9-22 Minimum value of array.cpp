e2764eb29fd82c5c1a60fca7d3fdebb96559f7f5
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-09-07 22:57:40 +0530

    Add files via upload

---
+++ 6-9-22 Minimum value of array.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100],size,i,min;
+  
+    cout<<"Enter the size of array : "<<endl;
+    cin>>size;
+    
+    cout<<"Enter the value in array : "<<endl;
+    for(i=0;i<size;i++)
+    {
+        cin>>a[i];
+    }
+    min=a[0];
+     for(i=0;i<size;i++)
+     {
+         if(a[i]<min)
+         {
+             min=a[i];
+         }
+     }
+     cout<<"#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100],size,i,max;
+  
+    cout<<"Enter the size of array : "<<endl;
+    cin>>size;
+    
+    cout<<"Enter the value in array : "<<endl;
+    for(i=0;i<size;i++)
+    {
+        cin>>a[i];
+    }
+    max=a[0];
+     for(i=0;i<size;i++)
+     {
+         if(a[i]>max)
+         {
+             max=a[i];
+         }
+     }
+     cout<<"Maximum value of array is "<<max;
+     
+    return 0;
+}
+
+
+ is "<<min;
+     
+    return 0;
+}
+
+
